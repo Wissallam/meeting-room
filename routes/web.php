a1ef06ac80e3ff0b60be008e2b073b1de516e2be
wissal lameghaizi <wissal@DESKTOP-PIOD45F.localdomain>
2022-07-28 10:59:37 +0100

    a multi step form

--- routes/web.php
+++ routes/web.php
@@ -30,15 +30,24 @@ use App\Http\Controllers\CalendarController;
 |
 */
 
+
+
+/*
+|--------------------------------------------------------------------------
+|                             Home page
+|--------------------------------------------------------------------------
+*/
+
 Route::get('/', function () {
     return view('welcome');
 });
 
+
 //test form calendar
 
 //Route::resource('/events',[EventController::class,'store']);
-Route::get('/events', function () {
-    return view('eventmodel');
+Route::get('/testform', function () {
+    return view('testform');
 });
 
 //test list of meetings for a user
@@ -61,7 +70,7 @@ Route::get('/listroom',[TestController::class,'index']);
 
 
 Route::get('/dashboard', function () {
-    return view('home');
+    return view('calender');
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
@@ -71,14 +80,7 @@ require __DIR__.'/auth.php';
 
 
 
-Route::get('/feature',[FeatureController::class,'index']);
-Route::get('/feature/new',[FeatureController::class,'new']);
-Route::post('/feature/save',[FeatureController::class,'save']);
-//print using dompdf package
-Route::get('/feature/print',[FeatureController::class,'print']);
-Route::get('/{id}/feature/edit', [FeatureController::class,'edit']);
-Route::post('/feature/update',[FeatureController::class,'update']);
-Route::get('/{id}/feature/delete',[FeatureController::class,'delete']);
+
 
 Route::get('/meeting',[MeetingController::class,'index']);
 Route::get('/meeting/new',[MeetingController::class,'new']);
@@ -89,14 +91,6 @@ Route::get('/{id}/meeting/edit', [MeetingController::class,'edit']);
 Route::post('/meeting/update',[MeetingController::class,'update']);
 Route::get('/{id}/meeting/delete',[MeetingController::class,'delete']);
 
-Route::get('/setting',[SettingController::class,'index']);
-Route::get('/setting/new',[SettingController::class,'new']);
-Route::post('/setting/save',[SettingController::class,'save']);
-//print using dompdf package
-Route::get('/setting/print',[SettingController::class,'print']);
-Route::get('/{id}/setting/edit', [SettingController::class,'edit']);
-Route::post('/setting/update',[SettingController::class,'update']);
-Route::get('/{id}/setting/delete',[SettingController::class,'delete']);
 
 
 
@@ -104,19 +98,20 @@ Route::get('/{id}/setting/delete',[SettingController::class,'delete']);
 
 
 
-Route::get('/photo',[PhotoController::class,'index']);
-Route::get('/photo/new',[PhotoController::class,'new']);
-Route::post('/photo/save',[PhotoController::class,'save']);
-Route::get('/{id}/photo/edit', [PhotoController::class,'edit']);
-Route::post('/photo/update',[PhotoController::class,'update']);
-Route::get('/{id}/photo/delete',[PhotoController::class,'delete']);
 
-// ------------- admin ------------------
+
+
+// ------------------------------------------------------------------------------
+// -------------------- admin -------------- admin--------------- admin-----------------------------
+// ---------------------------------------------------------------------------------
+
 
 Route::middleware(['auth', 'is_admin'])->group(function () {
 
+    //admin.dash
     Route::get('admin/dash', [DashController::class, 'admindash'])->name('admin.dash');
 
+    //departement
     Route::get('/departement',[DepartementController::class,'index']);
 Route::get('/departement/new',[DepartementController::class,'new']);
 Route::post('/departement/save',[DepartementController::class,'save']);
@@ -125,6 +120,7 @@ Route::get('/{id}/departement/edit', [DepartementController::class,'edit']);
 Route::post('/departement/update',[DepartementController::class,'update']);
 Route::get('/{id}/departement/delete',[DepartementController::class,'delete']);
 
+   // admin.user
     Route::get('/user',[UserController::class,'index']);
 Route::get('/user/new',[UserController::class,'new']);
 Route::post('/user/save',[UserController::class,'save']);
@@ -132,6 +128,7 @@ Route::get('/{id}/user/edit', [UserController::class,'edit']);
 Route::post('/user/update',[UserController::class,'update']);
 Route::get('/{id}/user/delete',[UserController::class,'delete']);
 
+   // role
     Route::get('/role',[RoleController::class,'index']);
 Route::get('/role/new',[RoleController::class,'new']);
 Route::post('/role/save',[RoleController::class,'save']);
@@ -139,6 +136,7 @@ Route::get('/{id}/role/edit', [RoleController::class,'edit']);
 Route::post('/role/update',[RoleController::class,'update']);
 Route::get('/{id}/role/delete',[RoleController::class,'delete']);
 
+   // admin.room
     Route::get('/room',[RoomController::class,'index']);
 Route::get('/room/new',[RoomController::class,'new']);
 Route::post('/room/save',[RoomController::class,'save']);
@@ -146,7 +144,7 @@ Route::get('/{id}/room/edit', [RoomController::class,'edit']);
 Route::post('/room/update',[RoomController::class,'update']);
 Route::get('/{id}/room/delete',[RoomController::class,'delete']);
 
-  //  Route::get('/photo',[PhotoController::class,'index']);
+  //  admin.photo
 Route::get('/photo/new',[PhotoController::class,'new']);
 Route::post('/photo/save',[PhotoController::class,'save']);
 Route::get('/{id}/photo/edit', [PhotoController::class,'edit']);
@@ -155,4 +153,23 @@ Route::get('/{id}/photo/delete',[PhotoController::class,'delete']);
 Route::get('/calendar', [CalendarController::class,'showcalendar']);
 Route::get('/calendar/new', [CalendarController::class,'newmeet']);
 
+  //feature
+   Route::get('/feature',[FeatureController::class,'index']);
+Route::get('/feature/new',[FeatureController::class,'new']);
+Route::post('/feature/save',[FeatureController::class,'save']);
+Route::get('/feature/print',[FeatureController::class,'print']);
+Route::get('/{id}/feature/edit', [FeatureController::class,'edit']);
+Route::post('/feature/update',[FeatureController::class,'update']);
+Route::get('/{id}/feature/delete',[FeatureController::class,'delete']);
+
+  //setting
+   Route::get('/setting',[SettingController::class,'index']);
+Route::get('/setting/new',[SettingController::class,'new']);
+Route::post('/setting/save',[SettingController::class,'save']);
+Route::get('/{id}/setting/edit', [SettingController::class,'edit']);
+Route::post('/setting/update',[SettingController::class,'update']);
+Route::get('/{id}/setting/delete',[SettingController::class,'delete']);
+
+
 });
+// ------------------------------------------------------------------------------
